10814a25825d7405945f9a5381ad72bae9b6bd78
Eltai01 <urmatov_e@iuca.kg>
2023-04-20 17:19:21 +0600

    k

--- app/src/main/java/com/example/dz5_2/MainActivity.java
+++ app/src/main/java/com/example/dz5_2/MainActivity.java
@@ -1,13 +1,33 @@
 package com.example.dz5_2;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnNext;
+    private EditText tvText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        btnNext = findViewById(R.id.bt_button);
+        tvText = findViewById(R.id.email_tv);
+        btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String text = tvText.getText().toString();
+                if (text.equals("ibrahim")) {
+                    Toast.makeText(MainActivity.this,
+                            "Eltai get", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
     }
 }
